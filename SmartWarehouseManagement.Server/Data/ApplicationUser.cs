465ca57aca0c1f280d2513983e8b7f1d20a0d9c2
thoni21 <90039692+thoni21@users.noreply.github.com>
2024-11-18 14:57:22 +0100

    Add project files.

---
+++ SmartWarehouseManagement.Server/Data/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace SmartWarehouseManagement.Server.Data
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
---
+++ smartwarehousemanagement.client/src/Pages/Home.tsx
@@ -0,0 +1,12 @@
+import LogoutLink from "../Components/LogoutLink.tsx";
+import AuthorizeView, { AuthorizedUser } from "../Components/AuthorizeView.tsx";
+
+function Home() {
+    return (
+        <AuthorizeView>
+            <span><LogoutLink>Logout <AuthorizedUser value="email" /></LogoutLink></span>
+        </AuthorizeView>
+    );
+}
+
+export default Home;
